b378b59853916052b8a69dc7eeff9861bc757a66
andreasschacht <schacht@kaliber5.de>
2017-11-23 19:31:36 +0100

    fix bc break of sf 3.3.13

--- SerializerBundle/Form/Extension/HttpFoundation/HttpFoundationRequestHandler.php
+++ SerializerBundle/Form/Extension/HttpFoundation/HttpFoundationRequestHandler.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\RequestHandlerInterface;
 use Symfony\Component\Form\Util\ServerParams;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -90,5 +91,10 @@ class HttpFoundationRequestHandler implements RequestHandlerInterface
             }
         }
         $form->submit($data, 'PATCH' !== $method);
+    }
+
+    public function isFileUpload($data)
+    {
+        return $data instanceof File;
     }
 }
