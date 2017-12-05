0e7236583e3bc647603dce0c22ca95b47db92ed0
andreasschacht <schacht@kaliber5.de>
2017-12-05 15:15:57 +0100

    add Countable-interface to PaginatedREpresentation

--- SerializerBundle/Hateoas/PaginatedRepresentation.php
+++ SerializerBundle/Hateoas/PaginatedRepresentation.php
@@ -9,15 +9,31 @@ use Webmozart\Assert\Assert;
 /**
  * Class PaginatedRepresentation
  */
-class PaginatedRepresentation extends BasePaginatedRepresentation implements \IteratorAggregate
+class PaginatedRepresentation extends BasePaginatedRepresentation implements \IteratorAggregate, \Countable
 {
     /**
      * @return \ArrayIterator
      */
     public function getIterator()
     {
+        return new \ArrayIterator($this->ensureCollection()->getResources());
+    }
+
+    /**
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->ensureCollection()->getResources());
+    }
+
+    /**
+     * @return CollectionRepresentation
+     */
+    protected function ensureCollection(): CollectionRepresentation
+    {
         Assert::isInstanceOf($this->getInline(), CollectionRepresentation::class);
 
-        return new \ArrayIterator($this->getInline()->getResources());
+        return $this->getinline();
     }
 }
